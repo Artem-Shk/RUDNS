b8ea11d5e59b78c7164abea7eaeee4c4854886f6
Artem <114388311+Artem-Shk@users.noreply.github.com>
2023-10-10 17:00:38 +0300

    Update Downloader.py

--- RutubeParser/Downloader.py
+++ RutubeParser/Downloader.py
@@ -1,24 +1,31 @@
 import requests
-from .CustomTypes.VideoInfo import VideoInfo
+from RutubeParser.CustomTypes.VideoInfo import VideoInfo
 
 import os
 
 class Downloader():
+    idVideo = 0
     @classmethod
     def download_img(self, video: VideoInfo):
-        name_file = video.image_url.split("/")[-1]
+        id = Downloader.idVideo.__str__()
+        raw_name_file = video.image_url.split("/")[-1]
+        format = raw_name_file.split('.')[1]
+        name_file = raw_name_file.split('.')[0] 
+
         catalog = video.catalog
+      
         if not os.path.exists(
            path_cotalog := f"res_img/{catalog}"
         ):
             os.makedirs(path_cotalog)
-   
-        
-        with open(f"res_img/{catalog}/{name_file}", "wb") as img:
+        with open(f"res_img/{catalog}/{name_file}_({id}).{format}", "wb") as img:
             img.write(
                 requests.get(video.image_url).content
             )
-        with open(f"res_img/{catalog}/{name_file}_meta.txt", "w", encoding="utf-8") as metainfo:
+      
+        with open(f"res_img/{catalog}/{name_file}_({id})_meta.txt", "w", encoding="utf-8") as metainfo:
             metainfo.write(
                 video.prompt
+            )
+        Downloader.idVideo +=1
             )
