4fb4273d2ad02eafbc06f4c9f0031c865d060ff3
Artem <114388311+Artem-Shk@users.noreply.github.com>
2023-10-10 18:08:51 +0300

    Update RutubeAPI.py

--- RutubeParser/RutubeAPI.py
+++ RutubeParser/RutubeAPI.py
@@ -1,12 +1,13 @@
 import requests
 import json
 import random
-from RutubeParser.CustomTypes.VideoInfo import VideoInfo
 
+from RutubeParser.CustomTypes.VideoInfo import VideoInfo
 
 class RutubeAPI:
 
     def __init__(self) -> None:
+        
         self.__category_list_url: str = "https://rutube.ru/api/video/category/?format=json"
         self.__videos_by_category_url: str = "http://rutube.ru/api/video/category/{}/?format=json&page={}"
         self.__info_video_url: str = "http://rutube.ru/api/video/{}/?format=json"
@@ -17,10 +18,11 @@ class RutubeAPI:
         self.__get_category_dict()
 
     def __get_category_dict(self) -> None:
-        category_list_response = self.__requests_session.get(self.__category_list_url)
+        category_list_response =self.__requests_session.get(self.__category_list_url)
         category_list_json_list = json.loads(category_list_response.content)
         for category in category_list_json_list: self.__category_dict.append(category["id"])
 
+    
     def get_info_by_url(self, url_video: str) -> VideoInfo:
         sp = url_video.split("/")
         data =  self.__requests_session.get(
